6acbc06edf0385699332385c8b059acc669d49be
B0086834 <824308644@qq.com>
2021-01-08 19:06:29 +0800

    sessiondemo   1.2

---
+++ src/cn/itcast/session/SessionDemo2.java
@@ -0,0 +1,29 @@
+package cn.itcast.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/sessionDemo2")
+public class SessionDemo2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //使用session共享数据
+
+        //获取session
+        HttpSession session = request.getSession();
+        //获取数据
+        Object attribute = session.getAttribute("msg");
+        System.out.println(attribute);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doPost(request,response);
+    }
+}
